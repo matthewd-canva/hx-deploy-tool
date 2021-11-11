309fdfe14afc0733c0f61cdc9f74d85f688de65b
Matthew Downey <matthew.d@canva.com>
2021-11-11 23:36:02 +1100

    Use docker for build, revert to old version of stack.yaml

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -7,15 +7,15 @@ USER=$(whoami)
 USERID=$(id -u)
 GROUPID=$(id -g)
 
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+USER_DIR="$(cd ~ && pwd)"
 
 docker run -it --rm \
- -v /home/$USER/.stack:/home/$USER/.stack \
+ -v $USER_DIR/.stack:/.stack \
  -v $ROOT:$ROOT \
  -v /etc/passwd:/etc/passwd \
  -v /etc/group:/etc/group \
  -v /tmp:/tmp \
- --user $USERID:$GROUPID \
  -w $ROOT \
  helixta/helixdev:2018-04-17 \
- /bin/bash -c "stack --no-nix build && cp `stack --no-nix exec which -- hx-deploy-tool` /tmp/hx-deploy-tool.$(arch)-linux && gzip -f /tmp/hx-deploy-tool.$(arch)-linux"
+ /bin/bash -c 'stack --no-nix --verbose build && cp `stack --no-nix exec which -- hx-deploy-tool` /tmp/hx-deploy-tool.$(arch)-linux && gzip -f /tmp/hx-deploy-tool.$(arch)-linux'
